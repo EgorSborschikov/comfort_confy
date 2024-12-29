c4ee5b6723d2299311b2a270af394194bdb3eab3
Igorik <egorsborsikov250575@gmail.com>
2024-12-29 12:46:06 +0700

    update register process

--- lib/services/registration_service/register_process.dart
+++ lib/services/registration_service/register_process.dart
@@ -1,22 +1,23 @@
 import 'package:comfort_confy/services/registration_service/register_user.dart';
 import 'package:comfort_confy/services/registration_service/user_create_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+// ignore: prefer_const_constructors
+final _storage = FlutterSecureStorage();
 
 Future<void> registerAndSave(UserCreateModel user) async {
   final token = await registerUser(user);
 
   // Сохранение состояния пользователя и JWT
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setBool('isRegistered', true);
-  await prefs.setString('jwtToken', token);
+  await _storage.write(key: 'isRegistered', value: 'true');
+  await _storage.write(key: 'jwtToken', value: token);
 }
 
 Future<bool> isUserRegistered() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getBool('isRegistered') ?? false;
+  final isRegistered = await _storage.read(key: 'isRegistered');
+  return isRegistered == 'true';
 }
 
 Future<String?> getJwtToken() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('jwtToken');
+  return await _storage.read(key: 'jwtToken');
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import file_selector_macos
 import firebase_analytics
 import firebase_core
+import flutter_secure_storage_macos
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
